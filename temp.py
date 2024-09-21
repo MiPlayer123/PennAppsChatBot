2ac008496a0e15b56d18614c0fcd6a723a6a7297
Mikul Saravanan <mikulsaravanan@gmail.com>
2024-09-21 02:17:13 -0400

    test

--- temp.py
+++ temp.py
@@ -2,17 +2,46 @@ from flask import Flask, render_template,jsonify,request
 from flask_cors import CORS
 import requests,openai,os
 from dotenv.main import load_dotenv
-from langchain.llms import OpenAI
+from langchain_community.llms import OpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationSummaryBufferMemory
-llm = OpenAI()
+from groq import Groq
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+client = Groq(
+    api_key="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" #os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "How are you?",
+        }
+    ],
+    model="llama3-8b-8192",
+)
+
+print(chat_completion.choices[0].message.content)
+
+print("API KEY : ")
+print(OPENAI_API_KEY)
+
+llm = OpenAI(
+    
+)
 memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=100)
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def index():
+    print("API KEY : ")
+    print(OPENAI_API_KEY)
     return render_template('index.html')
 
 @app.route('/data', methods=['POST'])
@@ -33,4 +62,5 @@ def get_data():
         return jsonify({"message":error_message,"response":False})
     
 if __name__ == '__main__':
+    print(chat_completion.choices[0].message.content)
     app.run()
